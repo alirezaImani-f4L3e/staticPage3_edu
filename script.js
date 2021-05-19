aeb602b3969a8be3083b8d3caf8afb82a8cb56dd
alirezaImani-f4L3e <83541857+alirezaImani-f4L3e@users.noreply.github.com>
2021-05-19 05:49:22 +0430

    first commit v-1

---
+++ script.js
@@ -0,0 +1,11 @@
+// var navBarBtn =document.getElementsByClassName('drop-down-menu')[0];
+
+
+window.onabort = function() {
+        alert("hello");
+    }
+    // function click() {
+    //     var aaa = document.getElementsByClassName('search-icon')[0];
+
+
+// }
